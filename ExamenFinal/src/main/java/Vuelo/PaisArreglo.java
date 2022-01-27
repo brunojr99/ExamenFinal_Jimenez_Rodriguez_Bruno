26b50e99d1f17c4b8806df1665a68a6f12bf63d2
brunojr99 <bruno.jimenez@unmsm.edu.pe>
2022-01-26 22:39:04 -0500

    7mo

--- ExamenFinal/src/main/java/Vuelo/PaisArreglo.java
+++ ExamenFinal/src/main/java/Vuelo/PaisArreglo.java
@@ -11,9 +11,10 @@ package Vuelo;
  */
 public class PaisArreglo {
     private int indice;
-
+    public static Pais[] arregloPaises;
     public PaisArreglo(int indice) {
         this.indice = indice;
+        this.arregloPaises = new Pais[50];
     }
 
     public int getIndice() {
@@ -25,14 +26,42 @@ public class PaisArreglo {
     }
     
     public boolean agregar(Pais p){
+        try{arregloPaises[this.indice]=p;
+        this.indice++;
+        return true;}
+        catch(Exception e){
         return false;
+    }
+        
     }
     
     public Pais getPais(String codigo){
+        int i= 0;
+        while(arregloPaises[i]!=null){
+            if(arregloPaises[i].getCodigo().equalsIgnoreCase(codigo)){
+                return arregloPaises[i];
+                
+            }
+            i++;
+        }
         return null;
     }
       
     public boolean eliminar(String codigo){
+        int i= 0;
+        while(arregloPaises[i]!=null){
+            if(arregloPaises[i].getCodigo().equalsIgnoreCase(codigo)){
+               while(arregloPaises[i+1]!=null){
+                   arregloPaises[i]= arregloPaises[i+1];
+                   i++;
+               }
+               arregloPaises[i]=null;
+               this.indice--;
+               return true;
+            }
+            i++;
+        }
+               
         return false;
     }
     
